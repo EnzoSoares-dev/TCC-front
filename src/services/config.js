359f456accd4ad102cd4abb3f749beb1bde4c29c
Enzo Soares <soaressenzo@gmail.com>
2025-05-20 20:36:28 -0300

    Fix: ajusta baseUrl config

--- src/services/config.js
+++ src/services/config.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const config = ()=>{
     return axios.create({
-        baseURL:"https://tcc-api-94yj.onrender.com",
+        baseURL:"https://tcc-api-red.vercel.app/",
         timeout: 10000,
     });
 }
